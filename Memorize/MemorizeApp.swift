7cd6473c35b417b0b5d0550e4507ae0c080a68a2
carloshenrique-dev <c.tarabal25@gmail.com>
2023-12-26 16:45:30 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Carlos Henrique on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
