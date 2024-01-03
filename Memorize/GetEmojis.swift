1c7ce756dfb6e0f7a4372dddc90998dce06f900d
carloshenrique-dev <c.tarabal25@gmail.com>
2024-01-03 20:16:26 -0300

    Victory alert + some refactor

---
+++ Memorize/GetEmojis.swift
@@ -0,0 +1,18 @@
+//
+//  GetEmojis.swift
+//  Memorize
+//
+//  Created by Carlos Henrique on 30/12/23.
+//
+
+import SwiftUI
+
+struct GetEmojis: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GetEmojis()
+}
